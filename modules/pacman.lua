828ec6104b445edf302b38812f992fc118c93b76
leandrogualter <leandrogualter@3d01d19b-eac9-46f1-e740-3a8d345c958b>
2011-09-03 03:40:09 +0000

    Correção de velocidade.

--- modules/pacman.lua
+++ modules/pacman.lua
@@ -7,34 +7,39 @@ pacman.ORIENTATION_UP = math.rad(270);
 pacman.ORIENTATION_DOWN = math.rad(90);
 pacman.ORIENTATION_LEFT = math.rad(180);
 pacman.ORIENTATION_RIGHT = math.rad(0);
-pacman.NORMAL_SPEED = 1;
-pacman.SUPER_SPEED = 2;
+pacman.NORMAL_SPEED = 100;
+pacman.SUPER_SPEED = 130;
 pacman.QUAD = love.graphics.newQuad(0,0,32,32,32,32);
 
 function pacman.reset()
   pacman.x = pacman.INITIAL_X;
   pacman.y = pacman.INITIAL_Y;
-  pacman.dead = false
-  pacman.super = false
+  pacman.speed = pacman.NORMAL_SPEED;
+  pacman.dead = false;
+  pacman.super = false;
   pacman.starting = true;
   pacman.orientation = pacman.ORIENTATION_RIGHT;
 end
 
 function pacman.update(dt)
+  function get_speed(dt)
+    return dt * pacman.speed;
+  end
+  
   if pacman.orientation == pacman.ORIENTATION_UP then
-    pacman.y = pacman.y - 1;
+    pacman.y = pacman.y - get_speed(dt);
   end
   
   if pacman.orientation == pacman.ORIENTATION_DOWN then
-    pacman.y = pacman.y + 1;
+    pacman.y = pacman.y + get_speed(dt);
   end
   
   if pacman.orientation == pacman.ORIENTATION_LEFT then
-    pacman.x = pacman.x - 1;
+    pacman.x = pacman.x - get_speed(dt);
   end
   
   if pacman.orientation == pacman.ORIENTATION_RIGHT then
-    pacman.x = pacman.x + 1;
+    pacman.x = pacman.x + get_speed(dt);
   end
 end
 
@@ -59,10 +64,6 @@ end
 function pacman.draw()
   love.graphics.drawq(spriteSheet, pacman.QUAD, pacman.x, pacman.y,
     pacman.orientation, 1, 1, pacman.width/2, pacman.height/2);
-end
-
-function pacman.initialize_quads()
-  
 end
 
 pacman.reset();
