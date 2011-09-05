b84f1a1bab9e21b18b1d9617439923244472805d
marcelosetimo@gmail.com <marcelosetimo@gmail.com@3d01d19b-eac9-46f1-e740-3a8d345c958b>
2011-09-05 23:06:41 +0000

    Criada animação para o pacman.

--- trunk/main.lua
+++ trunk/main.lua
@@ -1,5 +1,7 @@
 love.filesystem.load('assets/maps/tiledmap.lua')()
 love.filesystem.load('lib/TiledMapLoader.lua')()
+love.filesystem.load('lib/AnAL.lua')()
+Animations_legacy_support = true
 
 function love.load()
 	love.graphics.setMode(640, 480)
@@ -7,19 +9,21 @@ function love.load()
 	love.graphics.setBackgroundColor(0,0,0)
 	love.graphics.clear()
 	
-	spriteSheet = love.graphics.newImage('assets/sprites/spritesheet.png')
-	quadPacmanRight = love.graphics.newQuad(0,0,32,32,32,32)
+	pacmanSheet = love.graphics.newImage('assets/sprites/pacmansheet.png')
+	pacmanAnim = newAnimation(pacmanSheet, 32, 32, 0.04, 0)
+	pacmanAnim:setMode("bounce")
 	TiledMap_Load('assets/maps/tiledmap.tmx')	
 	require('modules.pacman');
 end
 	
 function love.draw()
   pacman.draw();
-	TiledMap_DrawNearCam(352,272)	
+  TiledMap_DrawNearCam(352,272)	
 end
 
 function love.update(dt)
   pacman.update(dt);
+  pacmanAnim:update(dt)
 end
 
 function love.keypressed(key, unicode)
